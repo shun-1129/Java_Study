8b919e81643c928507d65a1c5aebffb4b1225d58
Shunya Yokoi <88517400+shun-1129@users.noreply.github.com>
2022-04-13 14:37:59 +0900

    Day03

---
+++ SecondApplication/src/Jamet2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Jamet2 {
+
+	public static void main(String[] args) {
+		
+		double ave;
+		double a = 11.11;
+		double b = 33.33;
+		String ss;
+
+		opening();
+		ss = getstr();
+		putstr(ss);
+		ave = average(a, b);
+		putstr("average = " + ave);
+
+	}
+
+	static void opening() {
+		System.out.println("メソッドの動作を確認します");
+	}
+
+	static String getstr() {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+
+		return s;
+	}
+
+	static void putstr(String s) {
+		System.out.println(s);
+	}
+
+	static double average(double x, double y) {
+		return (x + y) / 2;
+	}
+
+}
